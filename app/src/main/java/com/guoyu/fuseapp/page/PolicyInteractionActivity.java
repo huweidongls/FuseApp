2a1f5945c2cd8909db7083caa655bafa4e144b84
1455426414 <39983872+1455426414@users.noreply.github.com>
2020-01-09 09:47:12 +0800

    9595959

---
+++ app/src/main/java/com/guoyu/fuseapp/page/PolicyInteractionActivity.java
@@ -0,0 +1,15 @@
+package com.guoyu.fuseapp.page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.guoyu.fuseapp.R;
+
+public class PolicyInteractionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_policy_interaction);
+    }
+}
